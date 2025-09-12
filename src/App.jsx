fcb931254156c60c92f7452d73bd58d714cc3a25
rao119349 <sauravyadav1193@gmail.com>
2025-09-12 20:10:12 +0530

    city added

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,47 @@ import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = `https://worldwiseappreactrouter-bhzs--9000--96435430.local-credentialless.webcontainer.io`;
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(false);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert('There was an error loading data...')
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities()
+  },[]);
+
   return (
     <BrowserRouter>
-    <Routes>
-      <Route index element = { <Homepage/> } />
-      <Route path="product" element = { <Product/> } />
-      <Route path="pricing" element = { <Pricing/> } />
-      <Route path="login" element = { <Login/> } />
-      <Route path="app" element = { <AppLayout/> }>
-        <Route index element = {<CityList/>}/>
-        <Route path="cities" element = { <CityList/> } />
-        <Route path="countries" element = { <p>List of countries here</p> } />
-        <Route path="form" element = { <p>Form</p> } />
-      </Route>
-      
-      {/* FOR PAGE NOT FOUND WE USE (*) */}
-      <Route path="*" element = { <PageNotFound/> } />
-    </Routes>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="countries" element={<p>List of countries here</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+
+        {/* FOR PAGE NOT FOUND WE USE (*) */}
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </BrowserRouter>
   )
 }
--- src/components/City.jsx
+++ src/components/City.jsx
@@ -18,7 +18,6 @@ function City() {
   };
 
   const { cityName, emoji, date, notes } = currentCity;
-
   return (
     <div className={styles.city}>
       <div className={styles.row}>
