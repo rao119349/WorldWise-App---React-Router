5ec4e55ff4d0d32897d8b4944b7d707a307c13e1
rao119349 <sauravyadav1193@gmail.com>
2025-09-26 19:11:49 +0530

    login functionality context

--- src/App.jsx
+++ src/App.jsx
@@ -11,29 +11,32 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 function App() {
   return (
-    <CitiesProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate replace to='cities' />} />
-          <Route path="cities" element={<CityList />} />
-          <Route path="cities/:id" element={<City />} />
-          <Route path="countries" element={<CountryList />} />
-          <Route path="form" element={<Form />} />
-        </Route>
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
+            <Route path="app" element={<AppLayout />}>
+              <Route index element={<Navigate replace to='cities' />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
 
-        {/* FOR all the pages except defined above & PAGE NOT FOUND WE USE (*) */}
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
-    </CitiesProvider>
+            {/* FOR all the pages except defined above & PAGE NOT FOUND WE USE (*) */}
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   )
 }
 
