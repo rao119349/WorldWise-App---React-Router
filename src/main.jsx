6406a24c0b17a231605ca8677fe85ebd597286ab
Saurav Yadav <sauravyadav1193@gmail.com>
2025-10-27 15:45:41 +0530

    insights added

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>
 );
